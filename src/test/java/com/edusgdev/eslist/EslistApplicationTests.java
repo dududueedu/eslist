c5307c7f3a1e6bd1d84bb433f8bd849cabd20a7b
dududueedu <eduardooffside@gmail.com>
2025-01-21 21:58:10 -0300

    Project Created

---
+++ src/test/java/com/edusgdev/eslist/EslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edusgdev.eslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
